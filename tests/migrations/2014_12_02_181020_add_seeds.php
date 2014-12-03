bf45040607a0ff849ba9f24fc585b79cc67111d5
Dimitrios Savvopoulos <ds@dimsav.com>
2014-12-03 17:09:50 +0100

    Added test category with children.

--- tests/migrations/2014_12_02_181020_add_seeds.php
+++ tests/migrations/2014_12_02_181020_add_seeds.php
@@ -13,10 +13,12 @@ class AddSeeds extends Migration {
 	public function up()
 	{
         $categories = [
-            ['id' => 1, 'parent_id' => null, 'lft' => 1, 'rght' => 8, 'name' => 'Sciences'],
-            ['id' => 2, 'parent_id' => 1, 'lft' => 2, 'rght' => 3, 'name' => 'Math'],
-            ['id' => 3, 'parent_id' => 1, 'lft' => 4, 'rght' => 5, 'name' => 'Medicine'],
-            ['id' => 4, 'parent_id' => 1, 'lft' => 6, 'rght' => 7, 'name' => 'Geology'],
+            ['id' => 1, 'parent_id' => null, 'lft' => 1, 'rght' => 12, 'name' => 'Sciences'],
+                ['id' => 2, 'parent_id' => 1, 'lft' => 2, 'rght' => 7, 'name' => 'Math'],
+                    ['id' => 3, 'parent_id' => 2, 'lft' => 3, 'rght' => 4, 'name' => 'Algebra'],
+                    ['id' => 4, 'parent_id' => 2, 'lft' => 5, 'rght' => 6, 'name' => 'Node theory'],
+                ['id' => 5, 'parent_id' => 1, 'lft' => 8, 'rght' => 9, 'name' => 'Medicine'],
+                ['id' => 6, 'parent_id' => 1, 'lft' => 10, 'rght' => 11, 'name' => 'Geology'],
         ];
 
         $this->createCategories($categories);
