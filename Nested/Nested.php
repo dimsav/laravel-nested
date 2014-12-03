72070f02cd625b829515b2ad7307673d3f0b6c00
Dimitrios Savvopoulos <ds@dimsav.com>
2014-12-03 13:35:58 +0100

    Added creating node without parent.

--- Nested/Nested.php
+++ Nested/Nested.php
@@ -2,4 +2,37 @@
 
 trait Nested {
 
+    protected $positions = ['after', 'before', 'inside'];
+
+    public function save(array $options = array())
+    {
+        if ( ! $this->exists)
+        {
+            if ($this->parent_id === null)
+            {
+                if ($last = $this->orderBy('rght', 'DESC')->first())
+                {
+                    $this->lft = $last->rght + 1;
+                    $this->rght = $last->rght + 2;
+                }
+                else
+                {
+                    // todo
+                }
+            }
+            else
+            {
+//                $parent = $this->find($this->parent_id);
+//
+//                if ($last = $this->orderBy('rght', 'DESC')->first())
+//                {
+//                    $this->lft = $last->rght + 1;
+//                    $this->rght = $last->rght + 2;
+//                }
+            }
+        }
+
+        return parent::save($options);
+    }
+
 }
